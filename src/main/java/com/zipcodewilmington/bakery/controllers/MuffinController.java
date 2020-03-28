c029a08acd806518830199fa4e7b22b225d16434
Ujjwal Shrestha <ushrestha@zipcodes-MBP.fios-router.home>
2020-03-27 22:21:35 -0400

    done

--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -2,12 +2,18 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.services.MuffinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 
 public class MuffinController {
     private MuffinService service;
 
+    @Autowired
     public MuffinController(MuffinService service) {
         this.service = service;
     }
@@ -16,19 +22,23 @@ public class MuffinController {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> show(Long id) {
+    @GetMapping("/muffins/{id}")
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> create(Muffin baker) {
+    @PostMapping("/muffins/")
+    public ResponseEntity<Muffin> create(@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Muffin> update(Long id, Muffin baker) {
+    @PutMapping("/muffins/{id}")
+    public ResponseEntity<Muffin> update(@PathVariable Long id, @RequestBody Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @DeleteMapping("/muffins/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
